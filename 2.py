4d3a66be6a4f8a6c0ae03dd5ca6616e9105ab113
rohitjoshi6 <rohitjoshi632001@gmail.com>
2021-01-03 17:09:39 +0530

    added soln for problem 2

---
+++ 2.py
@@ -0,0 +1,20 @@
+T=int(input())            
+letters="abcdefghijklmnop"
+for i in range(T):
+    N=int(input())
+    S=input()
+    if N>4:
+        out=""
+        parts = [S[i:i+4] for i in range(0, len(S), 4)]
+        for ele in parts:
+            ans=int(ele,2)
+            out=out+letters[ans]
+        print(out)    
+    else:
+        ans=int(S,2)
+        print(letters[ans])        
+
+
+
+
+

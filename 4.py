4e6b370fcea79b4f435b038ee3700ceb60b5ffe3
rohitjoshi6 <rohitjoshi632001@gmail.com>
2021-01-06 11:46:59 +0530

    AC

--- 4.py
+++ 4.py
@@ -17,5 +17,28 @@ for i in range(T):
                 ans.append(B[K-1])
 
 for i in range(T):
-    print(*ans[i])                
+    print(*ans[i]) 
+
+##########################################
+
+T=int(input())
+for x in range(T):
+    n,k,x,y=map(int,input().split(" "))
+    
+    if (x==y):
+        print(n,n)
+    else:
+        if (x>y):
+            col1=[n,y+(n-x)]
+            col2=[col1[1],n]
+            col3=[0,n-col2[0]]
+            col4=[col3[1],0]
+        else:
+            col1=[x+(n-y),n]
+            col2=[n,col1[0]]
+            col3=[n-col2[1],0]
+            col4=[0,col3[0]]
+        ans=[col4,col1,col2,col3]
+        res=ans[k%4]
+        print(res[0],res[1])
 
